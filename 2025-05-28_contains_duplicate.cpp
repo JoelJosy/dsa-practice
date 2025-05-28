9ec4108e3d0b5306cafc414dc1a0f052200b9f40
Joel Josy <joeljosy79@gmail.com>
2025-05-28 12:00:00 +0400

    Solved Contains Duplicate

---
+++ 2025-05-28_contains_duplicate.cpp
@@ -0,0 +1,15 @@
+# Contains Duplicate
+# https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int num: nums) {
+            if (set.count(num)) return true;
+            set.insert(num);
+        }
+
+        return false;
+    }
+};
