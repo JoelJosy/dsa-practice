b18e8c10436d135ed1ae5a78e586e7120d3ac0cd
Joel Josy <joeljosy79@gmail.com>
2025-06-30 11:47:57 +0400

    268.missing_number

---
+++ Arrays & Hashing/missing_number.cpp
@@ -0,0 +1,19 @@
+# Missing number
+# https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int given_sum = 0;
+        int real_sum = 0;
+        for (int num: nums) {
+            given_sum += num;
+        }
+        int n = nums.size();
+        for (int i = 0; i <= n; i++) {
+            real_sum += i;
+        }
+
+        return (real_sum - given_sum);
+    }
+};
