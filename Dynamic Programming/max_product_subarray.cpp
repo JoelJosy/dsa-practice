2f2e8e4827f37399f9d6f838adf6013b62dce121
Joel Josy <joeljosy79@gmail.com>
2025-07-07 15:00:28 +0400

    152 max subarray product

--- Dynamic Programming/max_product_subarray.cpp
+++ Dynamic Programming/max_product_subarray.cpp
@@ -1,3 +1,17 @@
 // 152. maximum product subarray
 // https://leetcode.com/problems/maximum-product-subarray/
 
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxProduct = nums[0];
+        int currProduct = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            currProduct = max(nums[i], currProduct * nums[i]);
+            maxProduct = max(maxProduct, currProduct);
+        }
+
+        return maxProduct;
+
+    }
+};
