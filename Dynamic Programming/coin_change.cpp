d50ca1205741d3dbf4e4e09de7c8816df1223b3c
Joel Josy <joeljosy79@gmail.com>
2025-07-07 13:28:45 +0400

    322 coin change top down

--- Dynamic Programming/coin_change.cpp
+++ Dynamic Programming/coin_change.cpp
@@ -1,6 +1,8 @@
 // 322 coin change
 // https://leetcode.com/problems/coin-change/
 
+// bottom up approach
+
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -27,3 +29,31 @@ public:
 
 // we can do dfs/backtracking but it will be slow
 // instead we can use dynamic programming to save repeated calculations
+
+// dfs + memo (top-down approach)
+class Solution {
+public:
+    unordered_map<int, int> memo; // map<currAmt, minCoinCount>
+    
+    int dfs(vector<int>& coins, int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+
+        if (memo.count(amount)) return memo[amount];
+
+        int res = INT_MAX;
+        for (int coin: coins) {
+            if (amount - coin >= 0) {
+                int next = dfs(coins, amount-coin);
+                if (next != -1)
+                res = min(res, 1 + dfs(coins, amount-coin));
+            }
+        }
+        memo[amount] = (res == INT_MAX) ? -1 : res;
+        return memo[amount];
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+       return dfs(coins, amount);
+    }
+};
