de0d79d40d27b3303227b79e4f4ca0cc6e0244bb
Joel Josy <joeljosy79@gmail.com>
2025-07-16 15:07:11 +0400

    reorder list

---
+++ Linked Lists/Reorder List.cpp
@@ -0,0 +1,48 @@
+// 143. Reorder List
+// https://leetcode.com/problems/reorder-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* second = slow->next; // second half of list
+        ListNode* first = head; // first half of list
+        slow->next = nullptr;
+
+        // second is accessed in reverse
+        ListNode* prev = nullptr;
+        while (second != nullptr) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+        second = prev;
+
+        // merge first and second
+        while (second != nullptr) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+        
+    }
+};
