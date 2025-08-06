3abb45819498d4fe35612d726d3b91f6572cab87
Joel Josy <joeljosy79@gmail.com>
2025-08-06 13:19:31 +0530

    sliding window maximum

---
+++ Sliding Window/sliding_window_max.cpp
@@ -0,0 +1,29 @@
+// 239. Sliding Window Maximum
+// https://leetcode.com/problems/sliding-window-maximum/
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> deq;
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            // pop old window values
+            while (!deq.empty() && deq.front() <= i - k) {
+                deq.pop_front();
+            }
+
+            // insert values if lesser 
+            // else pop until lesser
+            while (!deq.empty() && nums[i] > nums[deq.back()]) {
+                deq.pop_back();
+            }
+            deq.push_back(i);
+
+            // output deq front
+            if (i >= k - 1) {
+                res.push_back(nums[deq.front()]);
+            }
+        }
+        return res;
+    }
+};
