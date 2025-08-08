e0da01b39d2c6730e082adc77b4d9db5e16a448f
Joel Josy <joeljosy79@gmail.com>
2025-08-08 10:43:54 +0530

    same binary tree

---
+++ Trees/same_tree.cpp
@@ -0,0 +1,16 @@
+// http://www.leetcode.com/problems/same-tree/
+
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) {
+            return true;
+        }
+        if (p && q && p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        } else {
+            return false;
+        }
+    }
+};
