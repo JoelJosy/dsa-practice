b6bfadb900722729cd660b4d5275776988b7df5c
Joel Josy <joeljosy79@gmail.com>
2025-10-09 13:27:08 +0530

    house robber

---
+++ Dynamic Programming/house_robber.cpp
@@ -0,0 +1,21 @@
+// 198. House Robber
+// https://leetcode.com/problems/house-robber/
+
+class Solution {
+public:
+    vector<int> memo;
+    int rob(vector<int>& nums) {
+        memo.resize(nums.size(), -1);
+        dfs(nums, 0);
+    }
+
+    int dfs(vector<int>& nums, int i) {
+        if (i >= nums.size()) return 0;
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+
+        memo[i] = max(dfs(nums, i+1), nums[i] + dfs(nums, i+2));
+        return memo[i];
+    }
+};
