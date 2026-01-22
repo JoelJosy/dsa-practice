17fbcfa34cecd63d792179813925fb384d51f53a
Joel Josy <joeljosy79@gmail.com>
2026-01-22 12:46:43 +0530

    return array soln for max subarray

--- Dynamic Programming/max_subarray.cpp
+++ Dynamic Programming/max_subarray.cpp
@@ -31,3 +31,30 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    vector<int> maxSubArrayWithArray(vector<int>& nums) {
+        int maxSum = nums[0], currSum = 0;
+        int start = 0, end = 0, tempStart = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (currSum <= 0) {
+                // start new subarray
+                currSum = nums[i];
+                tempStart = i;
+            } else {
+                currSum += nums[i];
+            }
+
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                start = tempStart;  // record start of max subarray
+                end = i;            // record end of max subarray
+            }
+        }
+
+        vector<int> res(nums.begin() + start, nums.begin() + end + 1);
+        return res;
+    }
+};
