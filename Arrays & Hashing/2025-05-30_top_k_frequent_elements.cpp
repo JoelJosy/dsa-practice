2c287bd0fa936dc6f7604c02f9afe7577b7bf4f6
Joel Josy <joeljosy79@gmail.com>
2026-01-22 11:33:10 +0530

    min heap soln to top k frequent elements

--- Arrays & Hashing/2025-05-30_top_k_frequent_elements.cpp
+++ Arrays & Hashing/2025-05-30_top_k_frequent_elements.cpp
@@ -23,3 +23,37 @@ public:
     }
 
 };
+
+
+// Min heap 
+// O(nlogk)
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> res;
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Count : <num, freq>
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, 
+                        greater<pair<int, int>>> heap;
+
+        for (const auto& p: freq) {
+            heap.push({p.second, p.first});
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
+
+    }
+};
