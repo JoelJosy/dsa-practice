f4611a868c9ad92022c8288c8ea7058456c2997a
Joel Josy <joeljosy79@gmail.com>
2026-01-26 14:29:52 +0530

    lowest common ancestor, binary tree soln

--- Trees/lowest_common_ancestor.cpp
+++ Trees/lowest_common_ancestor.cpp
@@ -10,19 +10,49 @@
  * };
  */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || !p || !q) return nullptr;
-
-        if (max(p->val,q->val) < root->val) {
+        if (!root) return nullptr;
+        if (p->val < root->val && q->val < root->val) {
             return lowestCommonAncestor(root->left, p, q);
-        } 
-
-        if (min(p->val, q->val) > root->val) {
+        }
+        if (p->val > root->val && q->val > root->val) {
             return lowestCommonAncestor(root->right, p, q);
         }
-
         return root;
+    }
+};
+
+
+// Binary Tree soln
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return nullptr;
+        // if root is one of the nodes, it is LCA
+        if (root==p || root==q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        // if left and right non null, then p/q found
+        if (left && right) return root;
+
+        // if one is null, send back non null
+        return left ? left : right;
     }
 };
