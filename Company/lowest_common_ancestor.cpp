58557460b5c923ecaaff3b2991d391c7170b7036
Joel Josy <joeljosy79@gmail.com>
2026-02-18 22:50:58 +0530

    lowest common ancestor binary tree full soln w driver code

--- Company/lowest_common_ancestor.cpp
+++ Company/lowest_common_ancestor.cpp
@@ -1,3 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {} 
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {} 
+    TreeNode(int x, TreeNode* l, TreeNode* r): val(x), left(l), right(r) {} 
+};
 
 TreeNode* buildTree(vector<int>& arr) {
     if (arr.size() == 0 || arr[0] == -1) return nullptr;
@@ -5,22 +16,53 @@ TreeNode* buildTree(vector<int>& arr) {
     TreeNode* root = new TreeNode(arr[0]);
     q.push(root);
 
-    i=1;
-    while (!q.empty() && i < arr.size()) {
+    int i = 1;
+    while (i < arr.size() && !q.empty()) {
         TreeNode* node = q.front();
         q.pop();
 
-        if (i < arr.size() && arr[i] != -1) {
-            node->left = arr[i];
+        if (arr[i] != -1) {
+            node->left = new TreeNode(arr[i]);
             q.push(node->left);
         }
         i++;
 
-        if (i < arr.size() && arr[i] != -1) {
-            node->right = arr[i];
+        if (i<arr.size() && arr[i] != -1) {
+            node->right = new TreeNode(arr[i]);
             q.push(node->right);
         }
         i++;
     }
+
     return root;
+}
+
+void printTree(TreeNode* root) {
+    if (!root) cout << "Empty Tree";
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        cout << node->val << " "
+
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+    }
+}
+
+TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root) return nullptr;
+
+    if (root == p || root == q) return root;
+
+    TreeNode* left = lca(root->left, p, q);
+    TreeNode* right = lca(root->right, p, q);
+
+    if (left && right) return root;
+
+    return (left) ? left : right;
 }
