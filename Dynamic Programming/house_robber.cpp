88ac22429dc343d1554ea07fcecf899d586a1245
Joel Josy <joeljosy79@gmail.com>
2026-05-20 21:23:01 +0530

    house robber

--- Dynamic Programming/house_robber.cpp
+++ Dynamic Programming/house_robber.cpp
@@ -3,6 +3,26 @@
 
 class Solution {
 public:
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        if (nums.size() == 1) return nums[0];
+
+        // most amt at house 0, is to just rob it
+        dp[0] = nums[0];
+        dp[1] = max(dp[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            // if u dont rob, max is same as previous step
+            // if robbed, max is loot + max of 2 steps behind
+            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+        }
+
+        return dp[nums.size() - 1];
+    }
+};
+
+
+class Solution {
+public:
     vector<int> memo;
     int rob(vector<int>& nums) {
         memo.resize(nums.size(), -1);
