ee0827f1cac67847cf683385533cdef1a55ace68
Joel Josy <joeljosy79@gmail.com>
2026-05-23 10:11:39 +0530

    longest increasing subsequence memo + tabs

--- Dynamic Programming/longest_incr_subsequence.cpp
+++ Dynamic Programming/longest_incr_subsequence.cpp
@@ -1,6 +1,62 @@
 // 300. Longest Increasing Subsequence
 // https://leetcode.com/problems/longest-increasing-subsequence/
 
+// Memoization
+class Solution {
+public:
+    // index, prev -> count
+    int dfs(vector<int>& nums, int i, int prevIndex, vector<vector<int>>& memo) {
+        if (i == nums.size()) return 0;
+        
+        if (memo[i][prevIndex+1] != -1) {
+            return memo[i][prevIndex+1];
+        }
+
+        // leave it
+        int skip = dfs(nums, i+1, prevIndex, memo);
+
+        // take it
+        int take = skip;
+        if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
+            take = 1 + dfs(nums, i+1, i, memo);
+        }
+
+        return memo[i][prevIndex+1] = max(take, skip);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>> memo(nums.size(), vector<int>(nums.size(), -1)); 
+        return dfs(nums, 0, -1, memo);
+    }
+};
+
+// Tabulation
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        // When all processed, LIS is 0
+        // dp[i][prevIndex] -> LIS count
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int i = n-1; i >= 0; i--) {
+            for (int prevIndex = i-1; prevIndex >= -1; prevIndex--) {
+                // leave it
+                int skip = dp[i+1][prevIndex+1];
+
+                // take it
+                int take = skip;
+                if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
+                    take = 1 + dp[i+1][i+1];
+                }
+
+                dp[i][prevIndex+1] = max(take, skip);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
