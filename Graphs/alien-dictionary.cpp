eeeeebfeda5400cc1735b0b6a85f4a14bec4171c
Joel Josy <joeljosy79@gmail.com>
2026-05-29 12:25:21 +0400

    alien dictionary

---
+++ Graphs/alien-dictionary.cpp
@@ -0,0 +1,72 @@
+// Time: O(C+V+E) C - total num of chars across all words N*L
+// Space: O(V+E) graph and visited storage and recursion is O(V)
+
+class Solution {
+public:
+    // ["hrn","hrf","er","enn","rfnn"]
+    // n -> f
+    // h -> e
+    // r -> n
+    // e -> r
+    // "hernf"
+    bool dfs(char curr, string& res, unordered_map<char, unordered_set<char>>& graph, 
+    unordered_set<char>& visited, unordered_set<char>& visiting) {
+        // processed
+        if (visited.count(curr)) return true; 
+        // invalid if cycle
+        if (visiting.count(curr)) return false; 
+
+        visiting.insert(curr);
+        for (char n: graph[curr]) {
+            if (!dfs(n, res, graph, visited, visiting)) return false;
+        }
+        visiting.erase(curr);
+
+        visited.insert(curr);
+        res += curr;
+        return true;
+    }
+
+    string foreignDictionary(vector<string>& words) {
+        int n = words.size();
+        unordered_map<char, unordered_set<char>> graph;
+        for (string& word : words) {
+            // O(C) C - no of chars
+            // ensures char exists
+            for (char c : word) graph[c]; 
+        }
+
+        // build graph
+        for (int i = 0; i < n-1; i++) {
+            string w1 = words[i];
+            string w2 = words[i+1];
+            
+            if (w1.size() > w2.size() && w1.starts_with(w2)) 
+            return "";
+        
+            int minlen = min(w1.size(), w2.size());
+        
+            // O(C) - no of words
+            for (int j = 0; j < minlen; j++) {
+                if (w1[j] != w2[j]) {
+                    graph[w1[j]].insert(w2[j]);
+                    break;
+                }
+            }
+        }
+        unordered_set<char> visited;
+        unordered_set<char> visiting; // to check cycle (invalid)
+
+        string res = "";
+        
+        // traverse all components in case disconnected
+        // (multiple possibilities)
+        for (auto& [c, _] : graph) {
+            if (!dfs(c, res, graph, visited, visiting))
+                return "";
+        }
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
