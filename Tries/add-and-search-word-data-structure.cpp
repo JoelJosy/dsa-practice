598c59cac86f9591a8ca70bd82830b93addb5f3c
Joel Josy <joeljosy79@gmail.com>
2026-06-05 11:43:12 +0400

    implement trie with array

---
+++ Tries/add-and-search-word-data-structure.cpp
@@ -0,0 +1,4 @@
+// T: O(n)
+// S: O(t+n)
+// n -> length of string
+// t -> total number of nodes
--- Tries/implement-trie.cpp
+++ Tries/implement-trie.cpp
@@ -57,3 +57,65 @@ public:
         return true;
     }
 };
+
+// Using vector (slightly faster but more constant space)
+
+struct TrieNode {
+    TrieNode*  children(26, nullptr);
+    bool end = false;
+};
+
+class Trie {
+    TrieNode* root;
+public:
+    Trie() {
+        root= new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* cur=root;
+        for(char c:word){
+            int val=c-'a';
+            if(cur->children[val]==nullptr){
+                cur->children[val]=new TrieNode();
+            }
+            cur=cur->children[val];
+
+        }
+        cur->end=true;
+        
+    }
+    
+    bool search(string word) {
+        TrieNode* cur=root;
+        for(char c:word){
+             int val=c-'a';
+             if(cur->children[val]==nullptr){
+                return false;
+             }
+             cur=cur->children[val];
+        }
+            return cur->end;
+
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* cur=root;
+        for(char c:prefix){
+             int val=c-'a';
+             if(cur->children[val]==nullptr){
+                return false;
+             }
+             cur=cur->children[val];
+    }
+    return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
