c596a6f410b73684b07ad88522f68946eb776a24
Joel Josy <joeljosy79@gmail.com>
2026-06-09 12:50:42 +0400

    happy number fast and slow

--- Math/happy-number.cpp
+++ Math/happy-number.cpp
@@ -1,3 +1,31 @@
+// Space optimized O(1)
+class Solution {
+public:
+    int getsum(int n) {
+        int res = 0;
+        while (n > 0) {
+            int dig = n % 10;
+            n /= 10;
+            res += dig * dig;
+        }
+        return res;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = getsum(n);
+
+        while (fast != slow) {
+           slow = getsum(slow);
+           fast = getsum(fast);
+           fast = getsum(fast);
+        }
+
+        return fast == 1;
+    }
+};
+
+
 // Time and Space: O(logn)
 class Solution {
 public:
