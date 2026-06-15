9146fec6aa1a0d62fc5a499c7c89d98e18580b01
Joel Josy <joeljosy79@gmail.com>
2026-06-15 10:30:20 +0400

    task scheduler update

--- Heap_PQ/task_scheduler.cpp
+++ Heap_PQ/task_scheduler.cpp
@@ -52,6 +52,51 @@ public:
 
 
 // Max Heap
+// T: O(t) {O(tlogm but m is max 26 so log26 is constant)}
+// S: O(m)
+// t -> time
+// m -> distinct tasks
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char, int> freq;
+        for (char c: tasks) {
+            freq[c]++;
+        }
+
+        priority_queue<int> pq;
+        for (auto [c, f]: freq) {
+            pq.push(f);
+        }
+
+        // {remainingCount, timeAvailable}
+        queue<pair<int, int>> cooldown;
+
+        int time = 0;
+
+        while (!pq.empty() || !cooldown.empty()) {
+            time++;
+            
+            if (!cooldown.empty() && cooldown.front().second == time) {
+                pq.push(cooldown.front().first);
+                cooldown.pop();
+            }
+            
+            if (!pq.empty()) {
+                int freq = pq.top();
+                pq.pop();
+                freq--;
+                if (freq > 0)
+                    cooldown.push({freq, time + n + 1});
+            } 
+
+        }
+        return time;
+    }
+};
+
+
+
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
