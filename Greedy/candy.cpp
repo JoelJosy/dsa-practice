88af2dc4093dee337e9f46e63b5cc769fb06ba6a
Joel Josy <joeljosy79@gmail.com>
2026-06-27 12:55:03 +0400

    candy

---
+++ Greedy/candy.cpp
@@ -0,0 +1,27 @@
+// T: O(n)
+// S: O(n)
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+        
+        // left -> right
+        // cmp left neighbors
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i-1]) {
+                candies[i] = candies[i-1] + 1;
+            } 
+        }
+
+        // left <- right
+        // cmp right neighbors
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i+1]) {
+                candies[i] = max(candies[i], candies[i+1] + 1);
+            } 
+        }
+
+        return accumulate(candies.begin(), candies.end(), 0);
+    }
+};
