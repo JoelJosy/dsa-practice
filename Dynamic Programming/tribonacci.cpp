f861211a6bf55ed0385144a704555542d173df37
Joel Josy <joeljosy79@gmail.com>
2026-07-04 11:17:50 +0400

    tribonacci

---
+++ Dynamic Programming/tribonacci.cpp
@@ -0,0 +1,20 @@
+// T: O(n)
+// S: O(1)
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        int a = 0;
+        int b = 1, c = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+};
