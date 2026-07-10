629b6086f86073d36256471696a18729c48d2144
Joel Josy <joeljosy79@gmail.com>
2026-07-10 14:08:56 +0400

    path sum 3 dfs variant

--- Trees/path-sum-iii.cpp
+++ Trees/path-sum-iii.cpp
@@ -30,3 +30,39 @@ public:
 };
 
 // how many times have we seen sum equal to currpathsum - target 
+
+// T: O(n^2)
+// S: O(h) or O(n) worst case
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // count paths that must start at root
+    int dfs(TreeNode* root, int target) {
+        if (!root) return 0;
+
+        int count = 0;
+
+        if (root->val == target) count++;
+
+        count += dfs(root->left, target - root->val);
+        count += dfs(root->right, target - root->val);
+        return count;
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        return dfs(root, targetSum) +
+                pathSum(root->left, targetSum) +
+                pathSum(root->right, targetSum);
+    }
+};
