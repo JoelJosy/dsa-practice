bff5e0f61a8f921b48edc1a5ee962a77f2fd5e59
Joel Josy <joeljosy79@gmail.com>
2026-07-19 10:48:18 +0400

    max consecutive ones 3

---
+++ Sliding Window/max-consecutive-ones-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int l = 0, r = 0;
+        int maxlen = 0;
+        int flips = 0;
+        while (r < n) {
+            if (nums[r] == 0) {
+                flips++;
+            }
+
+            while (flips > k) {
+                if (nums[l] == 0)
+                    flips--;
+                l++;
+            }
+
+            maxlen = max(maxlen, r - l + 1);
+            r++;
+        }
+        return maxlen;
+    }
+};
