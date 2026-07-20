98379d8c130f674da48cae2583db8d896553a76c
Joel Josy <joeljosy79@gmail.com>
2026-07-20 12:15:22 +0400

    Check Completeness of a Binary Tree

---
+++ Trees/check-completeness-of-binary-tree.cpp
@@ -0,0 +1,25 @@
+// T: O(n)
+// T: O(n)
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        bool foundNull = false;
+        while (!q.empty()) {
+            TreeNode* node = q.front(); q.pop();
+
+            if (!node) {
+                foundNull = true;
+                continue;
+            }
+
+            if (foundNull) return false;
+
+            q.push(node->left);
+            q.push(node->right);
+        }
+        return true;
+    }
+};
